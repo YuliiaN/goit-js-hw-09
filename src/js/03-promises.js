a673e6627567602e05450a0f33b100e233d888ea
Yuliya N <y.sokolova.liya@gmail.com>
2023-01-10 17:32:21 +0200

    add third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const { Notify } = require('notiflix');
+
+const refs = {
+  form: document.querySelector('form'),
+  delay: document.querySelector('.js-delay'),
+  step: document.querySelector('.js-step'),
+  amount: document.querySelector('.js-amount'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  for (let i = 0; i < refs.amount.value; i++) {
+    let step = refs.step.value * i;
+
+    createPromise(i + 1, +refs.delay.value + step)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
